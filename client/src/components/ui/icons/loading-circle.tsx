71845e0e54ffe65c58981f0e68b043b116cc6097
Abhineet Anand <abhineetanand91@gmail.com>
2026-03-06 02:31:07 +0530

    front end deploy

--- client/src/components/ui/icons/loading-circle.tsx
+++ client/src/components/ui/icons/loading-circle.tsx
@@ -1,13 +1,8 @@
-import { cn } from "@dub/utils";
-
-export function LoadingCircle({ className }: { className?: string }) {
+export function LoadingCircle({ className = "" }: { className?: string }) {
     return (
         <svg
             aria-hidden="true"
-            className={cn(
-                "h-4 w-4 animate-spin fill-neutral-600 text-neutral-200",
-                className,
-            )}
+            className={`h-4 w-4 animate-spin fill-neutral-600 text-neutral-200 ${className}`}
             viewBox="0 0 100 101"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
